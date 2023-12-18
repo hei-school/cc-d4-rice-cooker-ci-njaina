993f1c1457936c2c057404c9c10feabc40e9119d
njaina <hei.njaina.2@gmail.com>
2023-12-18 11:24:31 +0300

    update

--- src/Ricecooker.js
+++ src/Ricecooker.js
@@ -1,16 +1,12 @@
-// Importing the PromptSyncImpl function
 import PromptSyncImpl from "./PromptSyncImpl.js";
 
-// Function to show options and perform actions
 const ShowOption = async () => {
   const options = [1, 2, 3];
 
   const choice = await PromptSyncImpl(`Choose an option for the usage of your rice cooker (${options.join(', ')}): `);
 
-  // Convert the choice to a number
   const numericChoice = parseInt(choice, 10);
 
-  // Check the user's choice and perform actions accordingly
   switch (numericChoice) {
     case 1:
       console.log("Cooking rice...");
